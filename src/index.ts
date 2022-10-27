b46c5ca54907c45204adaa2855734c3d6a5d6ecb
Sebastian Dechant <sdechant@blockchains.com>
2022-10-27 09:28:45 +0200

    add safety net

--- src/index.ts
+++ src/index.ts
@@ -10,21 +10,26 @@ const PORT = 8080;
 const app = express();
 app.get('/1.0/identifiers/:did', async (req, res) => {
   let didDocument: EvanDIDDocument | EvanSidetreeDidDocument;
-  if (!/:0x[^:]+$/.test(req.params.did)) {
-    didDocument = await resolverSidetree.resolveDid(req.params.did);
-  } else if (req.params.did.startsWith('did:evan:testcore:')) {
-    didDocument = await resolverTestcore.resolveDid(req.params.did);
-  } else if (req.params.did.startsWith('did:evan:0x')) {
-    didDocument = await resolverCore.resolveDid(req.params.did);
-  }
+  try {
+    if (!/:0x[^:]+$/.test(req.params.did)) {
+      didDocument = await resolverSidetree.resolveDid(req.params.did);
+    } else if (req.params.did.startsWith('did:evan:testcore:')) {
+      didDocument = await resolverTestcore.resolveDid(req.params.did);
+    } else if (req.params.did.startsWith('did:evan:0x')) {
+      didDocument = await resolverCore.resolveDid(req.params.did);
+    }
 
-  if (didDocument) {
-    res.send(didDocument);
-  } else {
+    if (didDocument) {
+      res.send(didDocument);
+    } else {
+      res.sendStatus(404);
+    }
+  } catch(e) {
+    console.log(`error fetching did document ${e.message}`);
     res.sendStatus(404);
   }
 });
 
 app.listen(PORT, function () {
-  console.log(`evan.network Resolver driver active on port ${ PORT }...`)
+  console.log(`evan.network Resolver driver active on port ${ PORT }...`);
 });
